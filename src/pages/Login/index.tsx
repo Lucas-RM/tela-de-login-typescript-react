5ec0c14d161b58a8ef1102cf907862f320ea132a
Lucas Marcondes <lucas.marcondes36@gmail.com>
2024-10-18 17:40:41 -0300

    Preparação do Projeto com React Hook Form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,29 @@
+import { useForm } from "react-hook-form";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 
 import { Container, LoginContainer, Column, Spacing, Title } from "./styles";
 
 const Login = () => {
+  const { 
+    watch, 
+    control, 
+    formState: { errors, isValid } 
+  } = useForm();
+
+  const form = watch();
+
+  console.log(form);
+
   return (
     <Container>
       <LoginContainer>
         <Column>
           <Title>Login</Title>
           <Spacing />
-          <Input placeholder="Usuário" />
+          <Input name="email" placeholder="Email" control={ control } />
           <Spacing />
-          <Input placeholder="Senha" />
+          <Input name="password" placeholder="Senha" control={ control } />
           <Spacing />
           <Button title="Entrar" />
         </Column>
